c0f9159fc2f9cddcb48f7acb9a4c58d60696da07
Jared Schram <JSchram@bhdefense.local>
2019-09-09 12:12:21 -0400

    Added initial todo list for the app as well as created base architecture for what i think will make sense

---
+++ app.js
@@ -0,0 +1,34 @@
+/*
+-----------------TO-DOs:
+- DATA MODULE - 
+add data from above into internal data structure
+calculate new budget
+
+- UI MODULE - 
+get input values for the description/value fields
+after added to data structure then display on UI
+update UI with new budget
+
+- CONTROLLER MODULE - 
+Add event handler for entering expenses/deposits
+
+*/
+
+//Creating modules for various aspects of the app
+var budgetController = (function(){
+
+    //some code
+
+})();
+var uiController = (function(){
+
+    //some code
+
+})();
+
+//using different names for arguments to leave this controller more independant
+var appController = (function(budgetCtrl, UICtrl){
+
+    //some code
+
+})(budgetController, uiController);
