3bf3778a64660d475ff03bacbb262aa6f0b69c63
Jared Schram <JSchram@bhdefense.local>
2019-09-18 09:47:25 -0400

    created some event listeners, DOM object and start of getting all input values from the app

--- app.js
+++ app.js
@@ -21,14 +21,62 @@ var budgetController = (function(){
 
 })();
 var uiController = (function(){
-
-    //some code
-
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    };
+    return {
+        getInput: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value, // either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function(){
+            return DOMstrings;0
+        }
+    };
 })();
 
 //using different names for arguments to leave this controller more independant
 var appController = (function(budgetCtrl, UICtrl){
+    var setupEventListeners = function(){
+        var DOM = uiController.getDOMstrings();
+        document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+        document.addEventListener('keypress', function(e){
+            if(e.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
+    
+    var ctrlAddItem = function(){
+        // 1. Get input data
+        var input = uiController.getInput();
+        console.log(input);
+        // 2. add item to budget controller
 
-    //some code
+        // 3. add item to UI
 
+        // 4. Calculate new budget
+
+        // 5. display new budget to UI
+
+    }
+    return {
+        init: function(){
+            setupEventListeners();
+        }
+    }
 })(budgetController, uiController);
+
+
+
+
+
+
+
+
